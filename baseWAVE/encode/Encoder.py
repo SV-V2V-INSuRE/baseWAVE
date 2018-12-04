11dd9d9e3f1920c95957346a01e91365a28b0d4e
Mengqiao Liu <mengqial@andrew.cmu.edu>
2018-12-04 15:08:43 -0800

    abstract encoder

---
+++ baseWAVE/encode/Encoder.py
@@ -0,0 +1,12 @@
+# encode object to message content
+# decode message content to object
+
+class AbstractEncoder
+
+    @abstractmethod
+    def encode(obj):
+        return ""
+
+    @abstractmethod
+    def decode(str):
+        return ""
