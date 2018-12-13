a7a239dac93b13cfb2c6724635b4e7ad2cab7c81
Mengqiao Liu <mengqial@andrew.cmu.edu>
2018-12-12 22:16:04 -0800

    Defined hash for certificate

--- baseWAVE/structs/Certificate.py
+++ baseWAVE/structs/Certificate.py
@@ -1,9 +1,18 @@
 class Certificate:
-    def __init__(self):
-        pass
+    def __init__(self, version, issuer, key, signature):
+        self.version = version
+        self.issuer = issuer
+        self.key = key
+        self.signature = signature
+
+    def __eq__(self, other):
+        return self.version == other.version and \
+            self.issuer == other.issuer and \
+            self.key == other.key and \
+            self.signature == other.signature
+
+    def __hash__(self):
+        return hash((self.version, self.issuer, self.key, self.signature))
 
     def encode():
-        return ""
-
-    def getHash():
         return ""
